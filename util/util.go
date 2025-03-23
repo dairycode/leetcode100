d218e35ea094b20b9e21cfcd276b707d8bdf57fe
dairycode <liuzeyes@126.com>
2025-03-23 17:11:26 +0800

    opt

--- util/util.go
+++ util/util.go
@@ -13,15 +13,3 @@ func Max(a, b int) int {
 	}
 	return b
 }
-
-func SlicesEqual(a, b []int) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i := 0; i < len(a); i++ {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-	return true
-}
